2250a021ad75601872a5896e806af03be7cdc9aa
James Bach <qweet.ing@gmail.com>
2014-03-16 20:46:13 +0000

    Implemented 'Store' functionality

--- simple.rb
+++ simple.rb
@@ -11,25 +11,26 @@ class Application
   property :api_key,     APIKey, :key => true
 end
 
-class Data
+class ApplicationData
   include DataMapper::Resource
 
-  property :api_key,     APIKey, :key => true, :required => true
-  property :data_key,    String, :length => 20
-  property :data_values, String, :length => 100
+  property :id,          Serial, :key => true
+  property :api_key,     String, :required => true
+  property :data_key,    String, :length => 20 
+  property :data_value,  String, :length => 100
 end
 
 DataMapper.finalize
 
 Application.auto_upgrade!
-Data.auto_upgrade!
+ApplicationData.auto_upgrade!
 
 
 post '/' do
   # For debugging
   @key = Application.all
 
-  {"keys" => Array(@key)}.to_json
+  { "http_code" => 200, "keys" => Array(@key) }.to_json
 
 end
 
@@ -41,7 +42,22 @@ end
 
 post '/store' do
   # For creation
+  opts = JSON.parse(request.body.read) 
+  halt(404, 
+        {
+          "return_code" => 404, 
+          "reason" => "required value not found"
+        }.to_json
+      ) if !valid_request?(opts,['api_key', 'data_key', 'data_value'])
 
+  key = Application.get(opts["api_key"])
+  halt(404, {"return_code" => 404, "reason" => "key not found"}.to_json) if key.nil?
+
+  data = ApplicationData.create(:api_key => opts["api_key"], :data_key => opts["data_key"], :data_value => opts["data_value"])
+  data.save
+
+  { "http_code" => 200}.to_json
+  
 end
 
 put '/store' do
@@ -52,6 +68,24 @@ put '/store' do
   # Return error if key is provided but does not exist in our registered
   # applications database
 
+  opts = JSON.parse(request.body.read) 
+  halt(404, 
+        {
+          "return_code" => 404, 
+          "reason" => "required value not found"
+        }.to_json
+      ) if !valid_request?(opts,['api_key', 'data_key'])
+
+  key = Application.get(opts["api_key"])
+  halt(404, {"return_code" => 404, "reason" => "key not found"}.to_json) if key.nil?
+
+  data = ApplicationData.all(:api_key => opts["api_key"], :data_key => opts["data_key"])
+  #data.update(:api_key => opts["api_key"], :data_key => opts["data_key"], :data_value => opts["data_value"])
+  #data.save
+
+  data.to_json
+  #{ "http_code" => 200}.to_json
+ 
 end
 
 delete '/store' do 
@@ -73,10 +107,17 @@ get '/retrieve' do
   
   # Return error if key is provided but does not exist in our registered
   # applications database
-
+  
 end
 
 private
+
+def valid_request?(body, required=[:api_key])
+
+  return false if required.find { | value | body[value].nil? }
+  return true 
+
+end
 
 def is_valid_key?(key)
   # Search Application table for key.
